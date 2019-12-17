dd70c1070035983451c0043f90863930b21a0ce5
Shaktyka <k-i-s-u-n-j-a@list.ru>
2019-12-17 13:25:17 +0500

    Добавлен файл autoresize + инпут в разметку для режима проверки

--- js/expression.js
+++ js/expression.js
@@ -1,10 +1,17 @@
 // Разметка блока выражения
-const makeExpressionEl = (expr, gameRegime) => {
-  return `<div class="game__expr-wrap">
-    <span class="mult-1">${expr.mult_1}</span>
+const makeExpressionEl = (expr, gameMode) => {
+  return `<div class="game__expr-wrap expr">
+    <span class="mult-1 expr__mult-1">${expr.mult_1}</span>
     <span class="symbol-mult">x</span>
-    <span class="mult-2">${expr.mult_2}</span>
+    <span class="mult-2 expr__mult-2">${expr.mult_2}</span>
     <span class="symbol-result">=</span>
-    <span class="mult-result">${gameRegime === 'lesson' ? expr.response : ''}</span>
+    ${
+      gameMode === `exam` ?
+        `<label class="expr__res" for="result">
+           <input class="expr__res-field" type="text" name="result" id="result" value="" />
+        </label>`
+        :
+        `<span class="mult-result">${ expr.response || ''}</span>`
+    }
   </div>`.trim();
 };

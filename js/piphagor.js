dc9dccfe96ec9c3e5273b06c3db517a844dfd99d
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-01-02 19:28:11 +0500

    Заменяет апострофы бэктиками

--- js/piphagor.js
+++ js/piphagor.js
@@ -1,50 +1,50 @@
-const form = document.querySelector('.piphagor__form');
-const table = form.querySelector('.table');
-const inputs = form.querySelectorAll('.table__field');
-const firstInput = form.querySelector('.table__field');
-const resetBtn = form.querySelector('.piphagor__reset-btn');
+const form = document.querySelector(`.piphagor__form`);
+const table = form.querySelector(`.table`);
+const resetBtn = form.querySelector(`.piphagor__reset-btn`);
 
-// Добавить переход по Enter на следующий элемент (или стрелочками)
-// + генерацию таблицы 
+// Добавить переход стрелочками на следующий элемент
+// + генерацию таблицы
 // Добавить переходы по неправильным ответам и пустым ячейкам
 // Добавить проверку (при каждом прав-ном вводе) на все прав-ные ответы в таблице, чтобы показывать модалку с поздр-нием
 
 // Генерация случайного числа от min до max включительно
-const getRandomNumber = (min, max) => min + Math.floor(Math.random() * (max + 1 - min));
+const getRandomNumber = (min, max) => {
+  return min + Math.floor(Math.random() * (max + 1 - min))
+ };
 
-// Обработчик изменения значений в таблице 
+// Обработчик изменения значений в таблице
 const tableChangeEventListener = (evt) => {
   evt.preventDefault();
   const clickedInput = evt.target;
-  if (clickedInput.classList.contains('table__field')) {
+  if (clickedInput.classList.contains(`table__field`)) {
     const resp = Number(clickedInput.value);
 
     toastr.options = {
-      "newestOnTop": false,
-      "positionClass": "toast-bottom-center",
-      "preventDuplicates": true,
-      "showDuration": "300",
-      "hideDuration": "500",
-      "timeOut": "1500",
-      "showEasing": "swing",
-      "hideEasing": "linear",
-      "showMethod": "fadeIn",
-      "hideMethod": "fadeOut"
+      'newestOnTop': false,
+      'positionClass': 'toast-bottom-center',
+      'preventDuplicates': true,
+      'showDuration': '300',
+      'hideDuration': '500',
+      'timeOut': '1500',
+      'showEasing': 'swing',
+      'hideEasing': 'linear',
+      'showMethod': 'fadeIn',
+      'hideMethod': 'fadeOut'
     };
-    
+
     // Получаем данные инпута
     const row = clickedInput.dataset.row;
     const col = clickedInput.dataset.col;
     const multiplication = Number(row) * Number(col);
-    
+
     // Проверяем ответ и произведение данных
     if (resp === multiplication) {
-      clickedInput.classList.remove('error--piph');
-      clickedInput.classList.add('right');
+      clickedInput.classList.remove(`error--piph`);
+      clickedInput.classList.add(`right`);
       const compl = compliment_words[[getRandomNumber(0, compliment_words.length - 1)]];
       toastr.success(compl);
     } else {
-      clickedInput.classList.add('error--piph');
+      clickedInput.classList.add(`error--piph`);
       const error_mess = error_messages[[getRandomNumber(0, error_messages.length - 1)]];
       toastr.error(error_mess);
     }
@@ -97,24 +97,24 @@ const tableKeydownEventListener = (evt) => {
   const row = field.dataset.row;
   const col = field.dataset.col;
   const multiplication = Number(row) * Number(col);
-    
+
   // Проверяем ответ и произведение данных
   if (response === multiplication) {
-    field.classList.remove('error--piph');
-    field.classList.add('right');
+    field.classList.remove(`error--piph`);
+    field.classList.add(`right`);
     const compl = compliment_words[[getRandomNumber(0, compliment_words.length - 1)]];
     toastr.success(compl);
     focusNextField();
   } else {
-    field.classList.add('error--piph');
-    field.classList.remove('right');
+    field.classList.add(`error--piph`);
+    field.classList.remove(`right`);
     const error_mess = error_messages[[getRandomNumber(0, error_messages.length - 1)]];
     toastr.error(error_mess);
   }
 };
 
 // table.addEventListener('change', tableChangeEventListener); // ненужный метод, убрать потом
-table.addEventListener('keydown', tableKeydownEventListener);
+table.addEventListener(`keydown`, tableKeydownEventListener);
 
 // Обработчик отправки формы
 const formSubmitEventListener = (evt) => {
@@ -124,22 +124,30 @@ const formSubmitEventListener = (evt) => {
 // Обработчик клика по reset
 const resetBtnClickEventListener = (evt) => {
   initTable();
+  const inputs = form.querySelectorAll(`.table__field`);
   // Сбросить все классы с ячеек
   inputs.forEach((it) => {
-    if (it.classList.contains('error--piph')) {
-      it.classList.remove('error--piph');
-    } else if (it.classList.contains('right')) {
-      it.classList.remove('right');
+    if (it.classList.contains(`error--piph`)) {
+      it.classList.remove(`error--piph`);
+    } else if (it.classList.contains(`right`)) {
+      it.classList.remove(`right`);
     }
   });
 };
 
-form.addEventListener('submit', formSubmitEventListener);
-resetBtn.addEventListener('click', resetBtnClickEventListener);
+form.addEventListener(`submit`, formSubmitEventListener);
+resetBtn.addEventListener(`click`, resetBtnClickEventListener);
+
+// Генерация таблицы
+const generateTable = (cols = 10, rows = 10) => {
+  table.innerHTML = ``;
+};
 
 // Инициализация таблицы
 const initTable = () => {
-  firstInput.focus();
+  generateTable();
+  // const firstInput = form.querySelector('.table__field');
+  // firstInput.focus();
 };
 
 initTable();
